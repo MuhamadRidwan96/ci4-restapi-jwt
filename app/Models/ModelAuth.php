f5f6a54abaad2f8c0d435213d03daa01beaf69a2
Rdn-M <muhammadreadone96@gmail.com>
2022-03-15 15:31:45 +0700

    menambahkan model auhtentikasi

---
+++ app/Models/ModelAuth.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Model;
+
+use CodeIgniter\Model;
+use Exception;
+
+class ModelAuth extends Model{
+
+    protected $table = "authentikasi";
+    protected $primarykey = "id";
+    protected $allowedFields = ['username','email','password'];
+
+    function getEmail($email){
+        $builder = $this->table('authentikasi');
+        $data = $builder->where('email', $email)->first();
+
+        if(!$data){
+            throw new Exception("user tidak ditemukan");
+        } return $data;
+    }
+
+}
