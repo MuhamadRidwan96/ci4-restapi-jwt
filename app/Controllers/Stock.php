830e9d5e163b340e8e3b93ae5350fe6a6c870d01
Rdn-M <muhammadreadone96@gmail.com>
2022-03-16 21:50:40 +0700

    create controller stock.php

---
+++ app/Controllers/Stock.php
@@ -0,0 +1,90 @@
+<?php namespace App\Controllers;
+
+use CodeIgniter\API\ResponseTrait;
+use App\Model\ModelStock;
+
+class Stock extends BaseController{
+    
+    use ResponseTrait;
+    function __construct()
+    {
+        $this->model = new ModelStock();
+    }
+
+    public function index(){
+
+        $data = $this->model->orderBy('mm','asc')->findAll();
+        return $this->respond($data, 200);
+    }
+
+    public function create(){
+        //use this methode
+        //$data = [
+        //    'value' => $this->request->getVar('value'),
+        //    'value2' => $this->request->getVar('value2')
+        // ];
+        // or use the methode below
+        $data = $this->request->getPost();
+        if(!$this->model->save($data)){
+            return $this->fail($this->model->errors);
+        } else {
+            $response = [
+                'status' => 201,
+                'error'  => null,
+                'messages' => [
+                    'success' => 'data berhasil di buat'
+                ]
+                ];
+                return $this->respond($response);
+        }
+    }
+
+    public function show($id=null){
+        
+        $data = $this->model->where('id', $id)->findAll();
+        if(!$data){
+            return $this->failNotFound("data tidak ditemukan");
+        } else{
+            return $this->respond($data, 200);
+        }
+    }
+
+    public function update($id=null){
+
+        $data = $this->request->getRawInput();
+        $data['id'] = $id;
+
+        $isExcist = $this->model->where('id', $id)->findAll();
+        if(!$isExcist){
+            return $this->failNotFound("data tidak ditemukan");
+        }
+        if(!$this->model->save($data)){
+            return $this->fail($this->model->errors);
+        }
+        $response = [
+            'status' => 200,
+            'error'  => null,
+            'messages' => [
+                'success' => 'data berhasil di update'
+            ]
+            ];
+            return $this->respond($response);
+    }
+
+    public function delete($id=null){
+        $data =$this->model->where('id', $id)->findAll();
+        if($data){
+            $this->model->delete($id);
+            $response = [
+                'status' => 200,
+                'error'  => null,
+                'messages' => [
+                    'success' => 'data berhasil di hapus'
+                ]
+                ];
+                return $this->respondDeleted($response);
+        } else {
+            return $this->failNotFound("data tidak ditemukan");
+        }
+    }
+}
