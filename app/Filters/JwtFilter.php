dd797d2214adf9aebb113e30cd91f2f88bdb9f65
Rdn-M <muhammadreadone96@gmail.com>
2022-03-16 21:01:42 +0700

    makes filters for token

---
+++ app/Filters/JwtFilter.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Filters;
+
+use CodeIgniter\HTTP\RequestInterface;
+use CodeIgniter\HTTP\ResponseInterface;
+use CodeIgniter\Filters\FilterInterface;
+use Config\Services;
+use Exception;
+
+class MyFilter implements FilterInterface
+{
+    public function before(RequestInterface $request, $arguments = null)
+    {
+        $header = $request->getServer('HTTP_AUTHORIZATION');
+        try{
+
+            helper('jwt');
+            $encodeToken = getJWT($header);
+            validateJWT($encodeToken);
+            return $request;
+        } catch(Exception $e){
+            return Services::response()->setJSON([
+                'error' => $e->getMessage()
+            ])->setStatusCode(ResponseInterface::HTTP_UNAUTHORIZED);
+        }
+    }
+
+    public function after(RequestInterface $request, ResponseInterface $response, $arguments = null)
+    {
+        // Do something here
+    }
+}
